99b87c197cf217ada393db0ae02365d2ba683334
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2023-04-23 23:16:43 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,54 @@
+
+
+function App() {
+  return (
+    <div className="App">
+     <header>
+		<h1>Prafulla Joshi</h1>
+		<p>Software developer</p>
+        <div class="links">
+        
+         <a href={"https://github.com/prafullajoshi970"}>Linkdn</a>
+        
+         <a href={"https://github.com/prafullajoshi970"}>Github</a>
+        </div>
+	</header>
+    <hr/>
+	<main>
+		<section>
+			<h2>Summary</h2>
+			<p>Highly motivated and results-driven software engineer with over 5 years of experience in developing web applications. Strong problem-solving skills and ability to work effectively in a team environment.</p>
+		</section>
+        <hr/>
+		<section>
+			<h2>Skills</h2>
+			<ul>
+				<li>Proficient in HTML, CSS, and JavaScript</li>
+				<li>Experience with ReactJs</li>
+				<li>Strong understanding of web development best practices</li>
+				<li>Excellent problem-solving and analytical skills</li>
+			</ul>
+		</section>
+        <hr/>
+		<section>
+			<h2>Experience</h2>
+			<h3>Software Intern</h3>
+			<p>June 2022 - Present</p>
+			<ul>
+				<li>Develop and maintain web applications using ReactJs</li>
+				<li>Collaborate with team members to identify and solve technical challenges</li>
+				<li>Contribute to the development of Realtime web Applications</li>
+			</ul>
+			
+		</section>
+        <hr/>
+		<section>
+			<h2>Education</h2>
+			<h3>Bachelor of Electrinics and Telecommunication, University of Pune</h3>
+          </section>
+		  </main>
+    </div>
+  );
+}
+
+export default App;
